7b3910af23b777d29b9a69aa8b60c43e6cd8fe9f
sagidev <sagithereal@gmail.com>
2022-04-06 00:56:57 +0200

    upload

---
+++ kolkoikrzyzyk/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace kolkoikrzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b7.Location = new System.Drawing.Point(12, 12);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(120, 120);
+            this.b7.TabIndex = 0;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b8.Location = new System.Drawing.Point(138, 12);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(120, 120);
+            this.b8.TabIndex = 1;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b9.Location = new System.Drawing.Point(264, 12);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(120, 120);
+            this.b9.TabIndex = 2;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b4.Location = new System.Drawing.Point(12, 138);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(120, 120);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b5.Location = new System.Drawing.Point(138, 138);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(120, 120);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b6.Location = new System.Drawing.Point(264, 138);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(120, 120);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(12, 264);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(120, 120);
+            this.b1.TabIndex = 6;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b2.Location = new System.Drawing.Point(138, 264);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(120, 120);
+            this.b2.TabIndex = 7;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b3.Location = new System.Drawing.Point(264, 264);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(120, 120);
+            this.b3.TabIndex = 8;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 391);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+    }
+}
+
---
+++ kolkoikrzyzyk/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kolkoikrzyzyk
+{
+    public partial class Form1 : Form
+    {
+        bool b1_g, b2_g, b3_g, b4_g, b5_g, b6_g, b7_g, b8_g, b9_g;  //gracz
+        bool b1_p, b2_p, b3_p, b4_p, b5_p, b6_p, b7_p, b8_p, b9_p;  //pc
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.R)
+            {
+                
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (b1_p == false)
+            {
+                b1_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            if (b2_p == false)
+            {
+                b2_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            if (b3_p == false)
+            {
+                b3_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            if (b4_p == false)
+            {
+                b4_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            if (b5_p == false)
+            {
+                b5_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            if (b6_p == false)
+            {
+                b6_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            if (b7_p == false)
+            {
+                b7_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            if (b8_p == false)
+            {
+                b8_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            if (b9_p == false)
+            {
+                b9_g = true;
+            }
+            Check();
+            enemyTurn();
+            winCheck();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Check()
+        {
+            if (b1_g == true) { b1.Text = "X"; }
+            if (b2_g == true) { b2.Text = "X"; }
+            if (b3_g == true) { b3.Text = "X"; }
+            if (b4_g == true) { b4.Text = "X"; }
+            if (b5_g == true) { b5.Text = "X"; }
+            if (b6_g == true) { b6.Text = "X"; }
+            if (b7_g == true) { b7.Text = "X"; }
+            if (b8_g == true) { b8.Text = "X"; }
+            if (b9_g == true) { b9.Text = "X"; }
+        }
+
+        private void enemyCheck()
+        {
+            if (b1_p == true) { b1.Text = "O"; }
+            if (b2_p == true) { b2.Text = "O"; }
+            if (b3_p == true) { b3.Text = "O"; }
+            if (b4_p == true) { b4.Text = "O"; }
+            if (b5_p == true) { b5.Text = "O"; }
+            if (b6_p == true) { b6.Text = "O"; }
+            if (b7_p == true) { b7.Text = "O"; }
+            if (b8_p == true) { b8.Text = "O"; }
+            if (b9_p == true) { b9.Text = "O"; }
+        }
+
+        private void winCheck()
+        {
+            if(b1_g == true && b2_g == true && b3_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b4_g == true && b5_g == true && b6_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b7_g == true && b8_g == true && b9_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b7_g == true && b4_g == true && b1_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b8_g == true && b5_g == true && b2_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b9_g == true && b6_g == true && b3_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b7_g == true && b5_g == true && b3_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b1_g == true && b5_g == true && b9_g == true)
+            {
+                MessageBox.Show("U won!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+        }
+
+        private void enemyWinCheck()
+        {
+            if (b1_p == true && b2_p == true && b3_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b4_p == true && b5_p == true && b6_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b7_p == true && b8_p == true && b9_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b7_p == true && b4_p == true && b1_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b8_p == true && b5_p == true && b2_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b9_p == true && b6_p == true && b3_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b7_p == true && b5_p == true && b3_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+            if (b1_p == true && b5_p == true && b9_p == true)
+            {
+                MessageBox.Show("U lost!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+        }
+
+        private void enemyTurn()
+        {
+            Random rng = new Random();
+            int roll;
+
+            roll = rng.Next(1, 10);
+
+            if(roll == 1 && b1_g == false) {b1_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 1 && b1_g == true){ enemyTurn(); }
+
+            if (roll == 2 && b2_g == false) {b2_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 2 && b2_g == true) { enemyTurn(); }
+
+            if (roll == 3 && b3_g == false) {b3_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 3 && b3_g == true) { enemyTurn(); }
+
+            if (roll == 4 && b4_g == false) {b4_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 4 && b4_g == true) { enemyTurn(); }
+
+            if (roll == 5 && b5_g == false) {b5_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 5 && b5_g == true) { enemyTurn(); }
+
+            if (roll == 6 && b6_g == false) {b6_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 6 && b6_g == true) { enemyTurn(); }
+
+            if (roll == 7 && b7_g == false) {b7_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 7 && b7_g == true) { enemyTurn(); }
+
+            if (roll == 8 && b8_g == false) {b8_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 8 && b8_g == true) { enemyTurn(); }
+
+            if (roll == 9 && b9_g == false) {b9_p = true; enemyCheck(); enemyWinCheck(); }
+            else if (roll == 9 && b9_g == true) { enemyTurn(); }
+
+        }
+    }
+}
